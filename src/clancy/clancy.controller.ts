602b32ca8b10ae903c46dbdcfe137a8052804254
hnamzian <namazian87@gmail.com>
2022-03-30 07:40:42 +0300

    Create API signatures

--- src/clancy/clancy.controller.ts
+++ src/clancy/clancy.controller.ts
@@ -1,17 +1,54 @@
-import { Controller, Post } from '@nestjs/common';
-import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
+import { Body, Controller, Get, Param, Post, Query, Req } from '@nestjs/common';
+import { ApiBearerAuth, ApiBody, ApiParam, ApiQuery, ApiTags } from '@nestjs/swagger';
 import { ClancyProvider } from './clancy.provider';
-import { GrantBySignatureDto } from './dto/clancy.dto';
+import { AddPresignerDto, GrantBySignatureDto, GrantRoleDto, HasRoleDto, RemovePresignerDto, RevokeRoleDto } from './dto/clancy.dto';
 
 @ApiTags('Clancy')
+@ApiBearerAuth('access-token')
 @Controller('clancy')
 export class ClancyController {
   constructor(private readonly clancyProvide: ClancyProvider) {}
 
-  @Post('/roles/grant-by-signature')
-  @ApiBearerAuth('access-token')
+  @ApiBody({ type: GrantRoleDto })
+  @Post('/grant-role')
+  async grantRole(@Body() grantRoleDto: GrantRoleDto, @Req() req) {}
+
+  @ApiBody({ type: RevokeRoleDto })
+  @Post('/revoke-role')
+  async revokeRole(@Body() revokeROleDto: RevokeRoleDto, @Req() req) {}
+
+
+  @Get('presigner')
+  async presigner() {}
+
+  @ApiParam({ name: 'account', type: String, example: '0x' })
+  @Get('/presigner/:account')
+  async getPresigner(@Param() account: string) {}
+
+  @ApiBody({ type: AddPresignerDto })
+  @Post('/add-presigner')
+  async addPresigner(@Body() addPresignerDto: AddPresignerDto, @Req() req) {}
+
+  @ApiBody({ type: RemovePresignerDto })
+  @Post('/add-presigner')
+  async removePresigner(@Body() removePresignerDto: RemovePresignerDto, @Req() req) {}
+
   @ApiBody({ type: GrantBySignatureDto })
-  async grantRoleBySignature(grantRoleBySignatureDto: GrantBySignatureDto) {
+  @Post('/roles/grant-by-signature')
+  async grantRoleBySignature(grantRoleBySignatureDto: GrantBySignatureDto, @Req() req) {
     return await this.clancyProvide.grantBySignature(grantRoleBySignatureDto);
   }
+
+  // @ApiQuery({ type: HasRoleDto })
+  @Get('/has-role')
+  async hasRole(@Query() hasRoleDto: HasRoleDto) {}
 }
+
+// grantRole
+// revokeRole
+// presigner()
+// getPresigner(address)
+// addPresigner(address)
+// removePresigner(address)
+// grantRoleBySignature(role(bytes32), account(address), signature(bytes32))
+// hasRole(role(bytes32), account(address))
--- src/clancy/dto/clancy.dto.ts
+++ src/clancy/dto/clancy.dto.ts
@@ -1,6 +1,62 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsString } from 'class-validator';
 
+export class GrantRoleDto {
+  @ApiProperty({
+    type: String,
+    description: 'Role to be granted',
+    example: '0x...',
+  })
+  @IsString()
+  role: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Account address',
+    example: '0x7C43CedB4C52262430193d4B0664016A7D0A2e07',
+  })
+  @IsString()
+  account: string;
+}
+
+export class RevokeRoleDto {
+  @ApiProperty({
+    type: String,
+    description: 'Role to be granted',
+    example: '0x...',
+  })
+  @IsString()
+  role: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Account address',
+    example: '0x7C43CedB4C52262430193d4B0664016A7D0A2e07',
+  })
+  @IsString()
+  account: string;
+}
+
+export class AddPresignerDto {
+  @ApiProperty({
+    type: String,
+    description: 'Role to be granted',
+    example: '0x...',
+  })
+  @IsString()
+  role: string;
+}
+
+export class RemovePresignerDto {
+  @ApiProperty({
+    type: String,
+    description: 'Role to be granted',
+    example: '0x...',
+  })
+  @IsString()
+  role: string;
+}
+
 export class GrantBySignatureDto {
   @ApiProperty({
     type: String,
@@ -25,4 +81,22 @@ export class GrantBySignatureDto {
   })
   @IsString()
   signature: string;
+}
+
+export class HasRoleDto {
+  @ApiProperty({
+    type: String,
+    description: 'Role to be granted',
+    example: '0x...',
+  })
+  @IsString()
+  role: string;
+
+  @ApiProperty({
+    type: String,
+    description: 'Account address',
+    example: '0x7C43CedB4C52262430193d4B0664016A7D0A2e07',
+  })
+  @IsString()
+  account: string;
 }
