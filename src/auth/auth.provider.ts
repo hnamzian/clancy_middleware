eeb253e2e3a8d5c5b9ea668b4cf48f1609432ad6
hnamzian <namazian87@gmail.com>
2022-03-31 16:39:10 +0430

    Remove initial admin creation

--- src/auth/auth.provider.ts
+++ src/auth/auth.provider.ts
@@ -3,7 +3,7 @@ import { UserProvider } from 'src/user/user.provider';
 import { LoginDto } from './dto/auth.dto';
 import { JwtService } from '@nestjs/jwt';
 import { AccessTokenPayload } from './types/jwtPayload';
-import { User, UserRole } from 'src/user/user.entity';
+import { Users, UserRole } from 'src/user/user.entity';
 
 @Injectable()
 export class AuthProvider {
@@ -11,29 +11,6 @@ export class AuthProvider {
     private readonly userProvider: UserProvider,
     private readonly jwtService: JwtService,
   ) {}
-
-  async onModuleInit() {
-    const admin = await this.createAdmin();
-    console.log(admin);
-  }
-
-  async createAdmin(): Promise<User> {
-    let admin = {
-      username: 'admin',
-      password: 'adminpw',
-      role: UserRole.admin,
-      isVerified: true,
-    } as User;
-    
-    let user = await this.userProvider.getUserByUsername('admin');
-
-    if (!user) {
-      const user = this.userProvider.createUser(admin);
-      return user;
-    }
-
-    return user;
-  }
 
   login = async (loginDto: LoginDto) => {
     const user = await this.userProvider.verifyCredentials(
@@ -55,7 +32,7 @@ export class AuthProvider {
   changePassword = async (
     oldPassword: string,
     newPassword: string,
-    user: User,
+    user: Users,
   ) => {
     await this.userProvider.updatePassword(user.id, oldPassword, newPassword);
   };
